065a8e8694c4954111a8df52b3d884adcf3ca0fd
Dmitrijv <dmitrij.vel@live.se>
2020-01-30 14:05:50 +0100

    added basic styling to index page

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
 
     // load shop items from json file
     const inventoryDir = "json/inventory.json";
-
     if (Modernizr.fetch) {
         fetch(inventoryDir).then(resp => resp.json()).then(json => console.log(json)).catch(err => console.error(err));
     } else {
