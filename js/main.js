da1bcb2f8117dbe9d0618fec51007ca0c42f2ffa
Dmitrijv <dmitrij.vel@live.se>
2020-01-30 14:00:42 +0100

    added modernizr check for fetch API

--- js/main.js
+++ js/main.js
@@ -3,7 +3,13 @@ $(document).ready(function () {
 
     // load shop items from json file
     const inventoryDir = "json/inventory.json";
-    fetch(inventoryDir).then(resp => resp.json()).then(json => console.log(json)).catch(err => console.error(err));
+
+    if (Modernizr.fetch) {
+        fetch(inventoryDir).then(resp => resp.json()).then(json => console.log(json)).catch(err => console.error(err));
+    } else {
+        // load the old school way
+    }
+
 
 
 });
