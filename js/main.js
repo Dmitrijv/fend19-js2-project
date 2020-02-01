8f5cbc2e5a704966fdbf12463a490324367f6987
Dmitrijv <dmitrij.vel@live.se>
2020-02-02 00:45:51 +0100

    add to cart buttons report item id to console

--- js/main.js
+++ js/main.js
@@ -9,22 +9,35 @@ $(document).ready(function() {
 });
 
 function loadStore(productsJson) {
-  //   console.log(productsJson);
   const productPanel = document.querySelector(".product-panel .panel-body");
+
+  // generate item HTML and append it to store panel
   let htmlPayload = ``;
   productsJson.forEach(item => {
     htmlPayload += `
     <div class="product-card">
-        <img class="img-fluid product-cover" src="img/product/product-${item.id}.jpg" alt="product${item.id}" />
-        <h4 id="product-${item.id}-name">${item.title}</h4>
-        <p id="product-2-description">${item.description}</p>
+        <img class="img-fluid product-cover" src="img/product/product-${item.id}.jpg" alt="${item.title}" />
+        <h4 id="product-name">${item.title}</h4>
+        <p>${item.description}</p>
         <hr />
-        <p class="product-price" id="prouct-2-price">${item.price.value} ${item.price.currency}</p>
+        <p class="product-price">${item.price.value} ${item.price.currency}</p>
         <div>
-        <button type="button" value="" class="btn btn-success">Add to cart</button>
-        <input type="number" min="1" max="1000" class="cart-item-qty" value="1" />
+        <button data-item-id="${item.id}" type="button" class="btn btn-success">Add to cart</button>
+        <input data-item-id="${item.id}" type="number" min="1" max="1000" class="cart-item-qty" value="1" />
         </div>
     </div>`;
   });
   productPanel.innerHTML = htmlPayload.length > 0 ? htmlPayload : "Failed to load store items.";
+
+  // add event listeners to "Add to cart" all buttons
+  const addToCartButtons = document.querySelectorAll(".product-panel .panel-body button[data-item-id]");
+  addToCartButtons.forEach(button => {
+    button.addEventListener("click", clickAddToCartButton);
+  });
+}
+
+function clickAddToCartButton(e) {
+  const thisButton = event.currentTarget;
+  const productID = thisButton.dataset.itemId;
+  console.log("Added product " + productID + " to cart");
 }
