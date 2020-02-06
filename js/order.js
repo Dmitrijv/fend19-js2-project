810009fbd6f63b264b90869915cae9a5c5770b75
Dmitrijv <dmitrij.vel@live.se>
2020-02-06 11:14:05 +0100

    cleaned up order timestamp generation

--- js/order.js
+++ js/order.js
@@ -1,12 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function() {
   const shoppingCart = JSON.parse(localStorage.getItem("shoppingCart"));
   const inventory = JSON.parse(sessionStorage.getItem("inventory"));
   let orderList = document.querySelector(".order-list");
   let subTotal = 0;
   let itemsCountTotal = 0;
-  let today = new Date();
-  let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate();
-  orderList.innerHTML = ``;
+  // orderList.innerHTML = ``;
 
   Object.keys(shoppingCart).forEach(itemID => {
     const item = inventory.find(item => item.id === Number(itemID));
@@ -34,9 +32,9 @@ $(document).ready(function () {
     </tbody>
     `;
 
-  document.querySelector('.totalPrice').textContent = subTotal + " kr";
+  document.querySelector(".totalPrice").textContent = subTotal + " kr";
   document.querySelector(".products-amount").innerHTML = itemsCountTotal;
-  document.querySelector('.dateToday').textContent = date;
+  document.querySelector(".dateToday").textContent = new Date().toLocaleString().slice(0, -6);
 
   const confirmButton = document.querySelector(".confirm-order-button");
   if (itemsCountTotal === 0) {
